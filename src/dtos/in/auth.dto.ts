c8d8e9e356b953a3b41add8077eb2b592d2d7683
quannhg <quan.nguyenhg12@gmail.com>
2023-11-25 11:52:58 +0700

    refactor(docker-compose): change DB port to environment variable for flexibility
    This refactor involves changing the port of the database to an environment variable for increased
    deployment flexibility. Additionally, the Swagger documentation example for the login API has been
    updated, and configurations for commitizen have been adjusted.

--- src/dtos/in/auth.dto.ts
+++ src/dtos/in/auth.dto.ts
@@ -3,9 +3,23 @@ import { Static, Type } from '@sinclair/typebox';
 
 // See https://github.com/sinclairzx81/typebox
 
-export const AuthInputDto = Type.Object({
-    email: Type.String({ minLength: MIN_EMAIL_LENGTH }),
-    password: Type.String({ minLength: MIN_PASSWORD_LENGTH })
-});
+export const AuthInputDto = Type.Object(
+    {
+        email: Type.String({ minLength: MIN_EMAIL_LENGTH }),
+        password: Type.String({ minLength: MIN_PASSWORD_LENGTH })
+    },
+    {
+        examples: [
+            {
+                email: 'admin@gmail.com',
+                password: 'admin1234'
+            },
+            {
+                email: 'customer@gmail.com',
+                password: 'user5678'
+            }
+        ]
+    }
+);
 
 export type AuthInputDto = Static<typeof AuthInputDto>;
