d6201dec61c0c184fa481a6f61a4575441c98cdd
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-12-18 18:18:32 +0700

    feat: order support query string

--- src/dtos/in/order.dto.ts
+++ src/dtos/in/order.dto.ts
@@ -1,3 +1,4 @@
+import { Status } from '@prisma/client';
 import { Static, Type } from '@sinclair/typebox';
 
 export const CreateOrderInputDto = Type.Object(
@@ -27,5 +28,47 @@ export const CreateOrderInputDto = Type.Object(
         ]
     }
 );
+
+export const OrderQueryStringDto = Type.Object({
+    created_after: Type.Optional(
+        Type.String({
+            format: 'date-time',
+            description: 'The time after which the order was created, specified in ISO format',
+            examples: ['2023-12-18']
+        })
+    ),
+    created_before: Type.Optional(
+        Type.String({
+            format: 'date-time',
+            description: 'The time before which the order was created, specified in ISO format',
+            examples: ['2023-12-25']
+        })
+    ),
+    isPaid: Type.Optional(Type.Boolean({ description: 'Filter on paid statud' })),
+    status: Type.Optional(
+        Type.Union(
+            Object.values(Status).map((e) => Type.Literal(e)),
+            { description: 'Filter on order status' }
+        )
+    ),
+    userId: Type.Optional(Type.String({ description: 'Filter on user id, only meaningful for managers' })),
+    start: Type.Optional(
+        Type.Number({ minimum: 0, multipleOf: 1, description: 'For pagination purpose - the index of the start item, starting at 0' })
+    ),
+    noItems: Type.Optional(
+        Type.Number({ minimum: 1, multipleOf: 1, description: 'For pagination purpose - the number of items to return' })
+    ),
+    orderBy: Type.Optional(
+        Type.Union([Type.Literal('totalPrice'), Type.Literal('shippingFee'), Type.Literal('creationTime')], {
+            description: 'The name of the field to order on',
+            examples: ['totalPrice', 'shippingFee', 'creationTime']
+        })
+    ),
+    order: Type.Optional(
+        Type.Union([Type.Literal('asc'), Type.Literal('desc')], { description: 'Sort ascending or descending', examples: ['asc', 'desc'] })
+    )
+});
+
+export type OrderQueryStringDto = Static<typeof OrderQueryStringDto>;
 
 export type CreateOrderInputDto = Static<typeof CreateOrderInputDto>;
