a5b62f2f1ee3585d82236422840f6b7a87d208a6
huydna <huy.dna@holistics.io>
2024-01-01 22:35:49 +0700

    fix: typescript unused var

--- src/utils/estimateShippingFee.ts
+++ src/utils/estimateShippingFee.ts
@@ -1,5 +1,5 @@
 // TODO: Implement estimateShippingFee properly
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function estimateShippingFee(order: { district: string; ward: string; street: string; streetNo: string }): number {
+export function estimateShippingFee(_order: { district: string; ward: string; street: string; streetNo: string }): number {
     return 30000;
 }
